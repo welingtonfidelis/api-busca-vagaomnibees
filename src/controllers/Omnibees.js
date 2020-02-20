0fbb4f087b3302bf06124d6428e821924ad76740
Welington Fidelis <welingtonfidelis@gmail.com>
2020-02-19 22:51:36 -0300

    Testes de busca

--- src/controllers/Omnibees.js
+++ src/controllers/Omnibees.js
@@ -6,8 +6,8 @@ module.exports = {
 
         console.log(CheckIn, CheckOut);
 
-        try {
-            const params = new URLSearchParams();
+        try {                
+            let params = new URLSearchParams();
             params.append('q', '5462');
             params.append('version', 'MyReservation');
             params.append('sid', 'b3295dd8-02c3-49c1-b546-c3aadea5495c#/');
@@ -21,11 +21,40 @@ module.exports = {
             params.append('ad', '1');
             params.append('ch', '0');
             params.append('ag', '-');
+            let resp = await axios.get("https://myreservations.omnibees.com/default.aspx", {params})
 
-            const resp = await axios.get("https://myreservations.omnibees.com/default.aspx", {params})
-            console.log(resp.data);
 
-            res.status(200).send({status: true,response: resp.data});
+            const now = new Date().getTime();
+            const sidPosition = (resp.data).search(/RoomSearch/)
+            let infos = (resp.data).substring(sidPosition+11, sidPosition+64);
+            infos = infos.replace(/['" ]+/g, '');
+            let sid = infos.split(',');
+            sid = sid[3]
+            // console.log(sid);
+            
+            params = new URLSearchParams();
+            params.append('ucUrl', 'SearchResultsByRoom')
+            params.append('diff', false)
+            params.append('CheckIn', CheckIn);
+            params.append('CheckOut', CheckOut);
+            params.append('Code', '')
+            params.append('group_code', '')
+            params.append('loyality_card', '')
+            params.append('NRooms', 1)
+            params.append('ad', 1)
+            params.append('ch', 0)
+            params.append('ag', '-')
+            params.append('q', 5462)
+            params.append('sid', sid)
+            params.append('rnd', now)
+            // const url = `https://myreservations.omnibees.com/Handlers/ajaxLoader.ashx?ucUrl=SearchResultsByRoom&diff=false&CheckIn=28022020&CheckOut=29022020&Code=&group_code=&loyality_card=&NRooms=1&ad=1&ch=0&ag=-&q=5462&sid=${sid}&rnd=${now}`;
+            const url = `https://myreservations.omnibees.com/Handlers/ajaxLoader.ashx`;
+            // console.log(url, now);
+            
+            resp = await axios.get(url, {params})
+            console.log(resp);
+   
+            res.status(200).send(resp.data);
 
         } catch (error) {
             const err = error.stack || error.errors || error.message || error;
