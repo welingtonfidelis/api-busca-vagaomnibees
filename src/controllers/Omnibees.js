d7b7bb66bbcbf80f58f4f629b94a3e96e916c523
Welington Fidelis <welingtonfidelis@gmail.com>
2020-02-20 22:24:31 -0300

    Separaçao em funçoes

--- src/controllers/Omnibees.js
+++ src/controllers/Omnibees.js
@@ -3,96 +3,36 @@ const cheerio = require('cheerio');
 
 module.exports = {
     async searchByDate(req, res) {
-        const { CheckIn, CheckOut } = req.body;
+        let { checkin, checkout, adults, childs, rooms, age } = req.body;
+
+        checkin = checkin.replace(/[\/-]/g, '');
+        checkout = checkout.replace(/[\/-]/g, '');
+        adults = adults ? adults : 1;
+        childs = childs ? childs : 0;
+        age = age ? age : '-0';
+        rooms = rooms ? rooms : 1;
+
+        console.log(`Entrada: ${checkin}, Saida: ${checkout}`);
+        console.log(`Qrt: ${rooms}, Adt: ${adults}, Cri: ${childs}, Idad: ${age}`);
 
         try {
-            //primeira requisição - recuperar id de sessão (sid)       
-            let params = new URLSearchParams();
-            params.append('q', '5462');
-            params.append('version', 'MyReservation');
-            params.append('diff', 'false');
-            params.append('CheckIn', CheckIn);
-            params.append('CheckOut', CheckOut);
-            params.append('Code', ' ');
-            params.append('group_code', ' ');
-            params.append('loyality_card', ' ');
-            params.append('NRooms', '1');
-            params.append('ad', '1');
-            params.append('ch', '0');
-            params.append('ag', '-');
-            let resp = await axios.get("https://myreservations.omnibees.com/default.aspx", {params});          
+            //Recupera o primeiro html para retirada dos ids de sessão (sid e aspSession)
+            let resp = await firstRequest();
 
-
-            //segunda requisição - recupera html com informações dos quartos
+            //retira sid e aspSession dos cookies
             const cookies = resp.headers['set-cookie'];
-            let aspSession = (cookies[1]+'').split(';');
+            let aspSession = (cookies[1] + '').split(';');
             aspSession = aspSession[0];
 
-            let sid = (cookies[2]+'').split('=');
+            let sid = (cookies[2] + '').split('=');
             sid = sid[1].replace('; expires', '');
-            console.log(`Entrada: ${CheckIn}, Saida: ${CheckOut}, SesId: ${sid}, AspSesi: ${aspSession}`);
 
-            params = new URLSearchParams();
-            params.append('ucUrl', 'SearchResultsByRoom')
-            params.append('diff', false)
-            params.append('CheckIn', CheckIn);
-            params.append('CheckOut', CheckOut);
-            params.append('Code', '')
-            params.append('group_code', '')
-            params.append('loyality_card', '')
-            params.append('NRooms', 1)
-            params.append('ad', 1)
-            params.append('ch', 0)
-            params.append('ag', '-')
-            params.append('q', 5462)
-            params.append('sid', sid)
+            //Recupera o segundo html para retirada das informações relevantes 
+            //ao usuário (tipo de quarto, descrição, preço, imagens)
+            resp = await secondRequest(checkin, checkout, adults, childs, age, rooms, sid, aspSession);
 
-            const headers = {
-                cookie: aspSession,
-            }
-            const url = `https://myreservations.omnibees.com/Handlers/ajaxLoader.ashx`;
-            resp = await axios.get(url, {params, headers})
-
-
-
-
-
-            //modo estático de requisição à segunda api
-            // let url = `https://myreservations.omnibees.com/Handlers/ajaxLoader.ashx?ucUrl=SearchResultsByRoom&&diff=false&CheckIn=${CheckIn}&CheckOut=${CheckOut}&Code=&group_code=&loyality_card=&NRooms=1&ad=1&ch=0&ag=-&q=5462&sid=1b4d1050-a9a6-45f9-93b1-2d385cd32732&rnd=1582237654784`;
-            // let resp = await axios.get(url, { headers: { cookie: 'ASP.NET_SessionId=5vaqmrg50bl3sq3bgxyxlnek' } });
-
-            const body = resp.data;
-            const $ = cheerio.load(body);
-            let statsTable = $('.maintable > tbody > .roomName');
-
-            const returnJson = [];
-            statsTable.each(function (index) {
-                let name = $(this).find('.excerpt > h5 > a').text();
-                let description = $(this).find('.excerpt > p > a').text();
-
-                const prices = [];
-                let priceHtml = $(`#jsRoom_${index}`);
-                priceHtml.each(function () {
-                    let price = $(this).find('.ratePriceTable > tbody > tr > td > a').text();
-                    prices.push(price);
-                });
-
-                const images = [];
-                let imgHtml = $(this).find('.thumb > .roomSlider > div');
-                imgHtml.each(function () {
-                    let image = 'https://myreservations.omnibees.com/' + $(this).find('.slide > a').prop('href');
-                    images.push(image);
-                });
-
-                console.log(' -> ', name, description, prices.length, imgHtml.length);
-
-                returnJson.push({
-                    name, description, prices, images
-                })
-            })
-
-
-            res.status(200).send(returnJson);
+            //Constrói json com dados retirados da segunda requisção e devolve ao cliente
+            res.status(200).send(createJsonResp(resp.data));
 
         } catch (error) {
             const err = error.stack || error.errors || error.message || error;
@@ -100,4 +40,67 @@ module.exports = {
             console.log(err);
         }
     }
+}
+
+//primeira requisição - recuperar id de sessão (sid e aspSession)       
+async function firstRequest() {
+    const params = new URLSearchParams();
+    params.append('q', '5462');
+    const url = "https://myreservations.omnibees.com/default.aspx";
+    return await axios.get(url, { params });
+}
+
+//segunda requisição - recupera html com informações dos quartos
+async function secondRequest(CheckIn, CheckOut, ad, ch, ag, NRooms, sid, aspSession) {
+    const params = new URLSearchParams();
+    params.append('ucUrl', 'SearchResultsByRoom');
+    params.append('diff', false);
+    params.append('CheckIn', CheckIn);
+    params.append('CheckOut', CheckOut);
+    params.append('Code', '');
+    params.append('group_code', '');
+    params.append('loyality_card', '');
+    params.append('NRooms', NRooms);
+    params.append('ad', ad);
+    params.append('ch', ch);
+    params.append('ag', ag);
+    params.append('q', 5462)
+    params.append('sid', sid);
+
+    const headers = {
+        cookie: aspSession,
+    }
+    const url = "https://myreservations.omnibees.com/Handlers/ajaxLoader.ashx";
+    return await axios.get(url, { params, headers });
+}
+
+//Buscar no html informações sobre os quartos (tipo, descrição, preço e imagens)
+//e construir um json para retornar ao cliente
+function createJsonResp(html) {
+    const $ = cheerio.load(html);
+    const statsTable = $('.maintable > tbody > .roomName');
+
+    const json = [];
+    statsTable.each(function (index) {
+        const name = $(this).find('.excerpt > h5 > a').text();
+        const description = $(this).find('.excerpt > p > a').text();
+
+        const prices = [], priceHtml = $(`#jsRoom_${index}`);
+        priceHtml.each(function () {
+            const price = $(this).find('.ratePriceTable > tbody > tr > td > a').text();
+            prices.push(price);
+        });
+
+        const images = [], imgHtml = $(this).find('.thumb > .roomSlider > div');
+        imgHtml.each(function () {
+            const image = 'https://myreservations.omnibees.com/' + $(this).find('.slide > a').prop('href');
+            images.push(image);
+        });
+
+        json.push({
+            name, description, prices, images
+        })
+    })
+
+    return json;
 }
